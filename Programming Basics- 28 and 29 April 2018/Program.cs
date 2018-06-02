6abe095a82cb28413cf557b90f5286258f27df04
Dilyana Gancheva <didikaraivanova@abv.bg>
2018-06-02 09:25:08 +0300

    Add files via upload

---
+++ Programming Basics- 28 and 29 April 2018/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Basics__28_and_29_April_2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sheetPrice = double.Parse(Console.ReadLine());
+            double coverPrice = double.Parse(Console.ReadLine());
+            double discountPaper = double.Parse(Console.ReadLine());
+            double designer = double.Parse(Console.ReadLine());
+            double discountTeam = double.Parse(Console.ReadLine());
+
+
+            double money = (sheetPrice * 899) + (coverPrice * 2);
+            double discountPaperTotal = discountPaper / 100.0;
+            double moneyBook = money * (1 - discountPaperTotal) + designer;
+            double discountTeamTotal = discountTeam / 100.0;
+
+            double totalMoney = moneyBook *( 1 - discountTeamTotal);
+            Console.WriteLine("Avtonom should pay {0:f2} BGN.", totalMoney);
+
+
+
+
+
+
+
+        }
+            
+    }
+}
