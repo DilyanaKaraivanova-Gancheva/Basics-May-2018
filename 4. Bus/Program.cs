c29b0c1d1e8c5401f0ed421995627e9918b799aa
Dilyana Gancheva <didikaraivanova@abv.bg>
2018-06-15 22:11:02 +0300

    Add files via upload

---
+++ 4. Bus/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Bus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int passingers = int.Parse(Console.ReadLine());
+            int station = int.Parse(Console.ReadLine());
+            int totalPassengers = 0;
+
+
+
+            for (int i = 1; i <= station; i++)
+            {
+                int down = int.Parse(Console.ReadLine());
+                int up = int.Parse(Console.ReadLine());
+                passingers += up;
+                passingers -= down;
+
+                if (i % 2 != 0)
+                {
+                    passingers += 2;
+                }
+                else
+                {
+                    passingers -= 2;
+                }
+            }
+            Console.WriteLine($"The final number of passengers is : {passingers}");
+        }
+    }
+}
