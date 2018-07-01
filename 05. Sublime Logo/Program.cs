4a6da3512d6cd6514a682438887dba1f0932e02f
Dilyana Gancheva <didikaraivanova@abv.bg>
2018-07-01 20:23:53 +0300

    Add files via upload

---
+++ 05. Sublime Logo/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Sublime_Logo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int width = 2 * n;
+            int space = width - 2;
+            int stars = width - space;
+
+            // TOP
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(new string(' ',space));
+                Console.WriteLine(new string('*',stars));
+                space -= 2;
+                stars += 2;
+            }
+            // AFTER TOP
+            Console.Write(new string('*', width - 2));
+            Console.WriteLine(new string(' ', 2));
+
+            // SECOND FOR++
+            stars = (width - 4);
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(new string('*',stars));
+                 stars += 2;
+            }
+
+            // MIDDLE ROW
+            Console.WriteLine(new string('*',width));
+
+            // SECOND FOR--
+            space = 2;
+            stars = width - space;
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write(new string(' ', space));
+                Console.WriteLine(new string('*',stars));
+                space += 2;
+                stars -= 2;
+            }
+
+            //LAST SINGLE ROW
+            Console.Write(new string(' ', 2));
+            Console.WriteLine(new string('*', width-2));
+
+            // BOTTOM
+            width = 2 * n;
+            stars = width; 
+            for (int i = n; i > 0; i--)
+            {
+                Console.WriteLine(new string('*', stars));
+                stars -= 2;
+
+            }
+
+        }
+    }
+}
